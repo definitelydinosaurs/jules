655bf2b0a30bf41db01d350156069ff255d05484
Randy Brown <thatrandybrown@gmail.com>
2025-08-07 23:11:27 -0500

    check for model existence

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+fn model_exists(model_path: &str) -> bool {
+  std::path::Path::new(&format!("{}/model.gguf", model_path)).exists()
+}
+
 fn main() {
   let args: Vec<String> = std::env::args().collect();
   if args.len() > 1 {
