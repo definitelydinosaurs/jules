16a076640a2bb7dd6ae8b58eacec06f37aca9a34
Randy Brown <thatrandybrown@gmail.com>
2025-10-06 21:32:54 -0500

    wrap invoke_llama_cli

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,15 @@
+mod jules;
+
+use jules::{model_exists, download_model, invoke_llama_cli};
+
+#[tauri::command]
+async fn prompt(prompt: String) {
+  match invoke_llama_cli(&prompt, true).await {
+    Ok(_) => println!("External process executed successfully"),
+    Err(e) => eprintln!("Error executing external process: {}", e),
+  };
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
